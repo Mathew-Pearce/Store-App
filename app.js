8862d7ca73fd7792d7dca2518240bf0bf7832cc7
Mathew-Pearce <matpearce1984@live.co.uk>
2024-02-21 10:12:06 +0000

    [Basic express server]

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log('04 Store API');
+require('dotenv').config();
+//async errors
+
+const express = require('express');
+const app = express();
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+//middlewares
+app.use(express.json());
+
+//Routes
+
+app.get('/', (req, res) => {
+  res.send('<h1>Store-API</h1><a href="/api/v1/products">Products Route</a>');
+});
+
+//Products Routes
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    app.listen(port, console.log(`Server started on port: ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
