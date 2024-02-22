6f2c27b60bebc57e7a1dfb68ba482518ec36d166
Mathew-Pearce <matpearce1984@live.co.uk>
2024-02-22 21:39:45 +0000

    [Sort]

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,12 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-  const search = 'ab'
-  const products = await Product.find({
-    name: { $regex: search, $options: 'i' },
-  })
+  const products = await Product.find({}).sort('-name -price')
   res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query
+  const { featured, company, name, sort } = req.query
   const queryObject = {}
   if (featured) {
     queryObject.featured = featured === `true` ? true : false
@@ -20,8 +17,17 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: 'i' }
   }
+  let result = Product.find(queryObject)
+  if (sort) {
+    const sortList = sort.split(',').join(' ')
+    result = result.sort(sortList)
+  } else {
+    result = result.sort('createdAt')
+  }
+
+  const products = await result
+
   console.log(queryObject)
-  const products = await Product.find(queryObject)
   res.status(200).json({ products, nbHits: products.length })
 }
 
