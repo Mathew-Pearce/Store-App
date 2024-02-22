3b8dd92c77435f9b434bce38f2653b825bcd6457
Mathew-Pearce <matpearce1984@live.co.uk>
2024-02-22 22:37:39 +0000

    [Select Option]

--- controllers/products.js
+++ controllers/products.js
@@ -1,28 +1,38 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort('-name -price')
+  const products = await Product.find({}).select('name price')
   res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query
+  const { featured, company, name, sort, fields } = req.query
   const queryObject = {}
+  //if featured is set to true, filter
   if (featured) {
     queryObject.featured = featured === `true` ? true : false
   }
+  //if company is set to true, filter
   if (company) {
     queryObject.company = company
   }
+  //if name is used as a filter then filter the products by name
   if (name) {
     queryObject.name = { $regex: name, $options: 'i' }
   }
+  //if sort is used then sort the products
   let result = Product.find(queryObject)
   if (sort) {
     const sortList = sort.split(',').join(' ')
     result = result.sort(sortList)
   } else {
     result = result.sort('createdAt')
+    //if fields is used then filter the fields
+  }
+  //Show only the selected fields
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ')
+    result = result.select(fieldsList)
   }
 
   const products = await result
